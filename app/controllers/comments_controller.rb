33731d849da5fa8deb79e37c3523c962368d7ba1
Ruby Dev <rubyplayer@Rubys-iMac.local>
2018-08-03 10:59:22 +0800

    Comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+    def create
+        @post = Post.find(param[:post_id])
+        @comment = @post.comments.create(param[:comment]).permit(:name, :comment)
+        redirect_to post_path(@post)
+    end
+
+
+    def destroy
+        
+    end
+end
